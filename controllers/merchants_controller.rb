add8b18d78096dbf69e793cb4094f9306a52655d
calumgould <calumgould26@gmail.com>
2020-02-10 10:36:10 +0000

    merchant edit

--- controllers/merchants_controller.rb
+++ controllers/merchants_controller.rb
@@ -7,3 +7,16 @@ get '/merchants' do
   @merchants = Merchant.all()
   erb(:"merchants/index")
 end
+
+#EDIT
+post '/merchants/:id' do
+  @merchant = Merchant.new(params)
+  @merchant.update()
+  redirect '/merchants'
+end
+
+#EDIT
+get '/merchants/:id' do
+  @merchant = Merchant.find(params['id'].to_i())
+  erb(:"merchants/edit")
+end
